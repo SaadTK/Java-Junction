1e4947953790f4ec3401893d033858de7b8e624f
SaadTK <tahmid.karim.saad@gmail.com>
2025-05-14 22:26:56 +0600

    finished basic of MainLayout

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,30 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./components/Home";
+import AddCoffee from "./components/AddCoffee";
+import UpdateCoffee from "./components/UpdateCoffee";
 import { createBrowserRouter, RouterProvider } from "react-router";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "/addCoffee",
+        Component: AddCoffee,
+      },
+      {
+        path: "/updateCoffee",
+        Component: UpdateCoffee,
+      },
+    ],
   },
 ]);
 
--- server/index.js
+++ server/index.js
@@ -1,11 +1,49 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use(dotenv.config())
+dotenv.config();
 app.use(cors());
 app.use(express.json());
+
+// tahmidkarimsaad: elzvSla5PeVYcmmO
+
+// mongoDB part start
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@jjcluster.12jkxtp.mongodb.net/?retryWrites=true&w=majority&appName=JJCluster`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+    console.log("Finally I am on.");
+  }
+}
+run().catch(console.dir);
+
+// mongoDB part end
 
 app.get("/", (req, res) => {
   res.send("Coffee server is being kept warm.");
